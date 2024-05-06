a4b266734a05c480cb8a53bcbb7744329b3659e7
DavidLopezIT1 <santyfcw@gmail.com>
2024-05-05 23:29:42 -0500

    WhileStructure Understod

--- whileStructures.py
+++ whileStructures.py
@@ -1,4 +1,10 @@
-numero =2
-while numero <= 18:
-    print(numero)
-    numero+=1
+# numero =2
+# while numero <= 18:
+#     print(numero)
+#     numero+=1
+
+var = ""
+while var.lower() !="salir":
+    var = input("")
+    print(var.lower())
+
